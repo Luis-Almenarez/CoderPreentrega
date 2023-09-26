414ebe79d8e75cd9c156ec5cf0d8c0d26f3991e3
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-09-26 17:08:00 -0500

    -

--- Entrega2/app.js
+++ Entrega2/app.js
@@ -11,61 +11,63 @@ let iniciarJuego = () => {
       2- Papel
       3- Tijeras `);
 
-  let usuarioTexto;
+  while ((puntosUsuario = 5 || (puntosComputadora = 5))) {
+    let usuarioTexto;
 
-  if (opcionUsuario !== null) {
-    const opcionUsuarioNum = parseInt(opcionUsuario);
+    if (opcionUsuario !== null) {
+      const opcionUsuarioNum = parseInt(opcionUsuario);
 
-    if (opcionUsuarioNum >= 1 && opcionUsuarioNum <= 3) {
-      if (opcionUsuarioNum === 1) {
-        usuarioTexto = "Piedra";
-      } else if (opcionUsuarioNum === 2) {
-        usuarioTexto = "Papel";
-      } else if (opcionUsuarioNum === 3) {
-        usuarioTexto = "Tijeras";
+      if (opcionUsuarioNum >= 1 && opcionUsuarioNum <= 3) {
+        if (opcionUsuarioNum === 1) {
+          usuarioTexto = "Piedra";
+        } else if (opcionUsuarioNum === 2) {
+          usuarioTexto = "Papel";
+        } else if (opcionUsuarioNum === 3) {
+          usuarioTexto = "Tijeras";
+        }
+      } else {
+        alert("Opción no válida. Por favor, elige 1, 2 o 3.");
+        location.reload();
       }
-    } else {
-      alert("Opción no válida. Por favor, elige 1, 2 o 3.");
-      location.reload();
     }
-  }
 
-  if (opcionComputadora === 0) {
-    opcionComputadora = "Piedra";
-  } else if (opcionComputadora === 1) {
-    opcionComputadora = "Papel";
-  } else if (opcionComputadora === 2) {
-    opcionComputadora = "Tijeras";
-  }
+    if (opcionComputadora === 0) {
+      opcionComputadora = "Piedra";
+    } else if (opcionComputadora === 1) {
+      opcionComputadora = "Papel";
+    } else if (opcionComputadora === 2) {
+      opcionComputadora = "Tijeras";
+    }
 
-  console.log("La computadora ha usado " + opcionComputadora);
-  console.log("Usuario ha utilizado " + usuarioTexto);
+    console.log("La computadora ha usado " + opcionComputadora);
+    console.log("Usuario ha utilizado " + usuarioTexto);
 
-  // Gana usuario
-  if (
-    (usuarioTexto === "Piedra" && opcionComputadora === "Tijeras") ||
-    (usuarioTexto === "Papel" && opcionComputadora === "Piedra") ||
-    (usuarioTexto === "Tijeras" && opcionComputadora === "Papel")
-  ) {
-    puntosUsuario++;
-    console.log("Haz ganado la ronda");
+    // Gana usuario
+    if (
+      (usuarioTexto === "Piedra" && opcionComputadora === "Tijeras") ||
+      (usuarioTexto === "Papel" && opcionComputadora === "Piedra") ||
+      (usuarioTexto === "Tijeras" && opcionComputadora === "Papel")
+    ) {
+      puntosUsuario++;
+      console.log("Haz ganado la ronda");
 
-    //Gana Computadora
-  } else if (
-    (opcionComputadora === "Piedra" && usuarioTexto === "Tijeras") ||
-    (opcionComputadora === "Papel" && usuarioTexto === "Piedra") ||
-    (opcionComputadora === "Tijeras" && usuarioTexto === "Papel")
-  ) {
-    puntosComputadora++;
-    console.log("La computadora ha ganado la ronda");
-  } else {
-    console.log("Empate");
-  }
+      //Gana Computadora
+    } else if (
+      (opcionComputadora === "Piedra" && usuarioTexto === "Tijeras") ||
+      (opcionComputadora === "Papel" && usuarioTexto === "Piedra") ||
+      (opcionComputadora === "Tijeras" && usuarioTexto === "Papel")
+    ) {
+      puntosComputadora++;
+      console.log("La computadora ha ganado la ronda");
+    } else {
+      console.log("Empate");
+    }
 
-  if (puntosUsuario === 5) {
-    GanaUsuario();
-  } else if (puntosComputadora === 5) {
-    GanaComputadora();
+    if (puntosUsuario === 5) {
+      GanaUsuario();
+    } else if (puntosComputadora === 5) {
+      GanaComputadora();
+    }
   }
 };
 
