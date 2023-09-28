5441d4e26a84c5780dfa63efcd95bcc867955b99
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-09-28 18:53:32 -0500

    Comentando código JS

--- Entrega2/app.js
+++ Entrega2/app.js
@@ -1,10 +1,15 @@
+// Declaración de variables para llevar el registro de puntos y rondas.
 let puntosUsuario = 0;
 let puntosComputadora = 0;
 
+// Arrays para registrar el historial de jugadas.
 let historialUsuaio = [];
 let historialComputadora = [];
+
+// Contador de rondas.
 let contadorRondas = 0;
 
+// Objeto que define las reglas del juego.
 const resultados = {
   Piedra: {
     nombre: "Piedra",
@@ -20,9 +25,12 @@ const resultados = {
   },
 };
 
+// Función principal para iniciar el juego.
 let iniciarJuego = () => {
   while (puntosUsuario !== 5 && puntosComputadora !== 5) {
+    // Genera una elección aleatoria para la computadora.
     let opcionComputadora = Math.floor(Math.random() * 3);
+    // Solicita al usuario su elección.
     let opcionUsuario = prompt(`
     El primero en llegar a 5 puntos ganará la partida
 
@@ -34,6 +42,7 @@ let iniciarJuego = () => {
     contadorRondas++;
     console.log("Ronda " + contadorRondas);
 
+    // Valida la elección del usuario.
     if (opcionUsuario !== null) {
       const opcionUsuarioNum = parseInt(opcionUsuario);
 
@@ -47,10 +56,11 @@ let iniciarJuego = () => {
         }
       } else {
         alert("La opción seleccionada no es válida. Por favor, elige 1, 2 o 3.");
-        continue;
+        continue; // Continúa con la siguiente iteración del bucle.
       }
     }
 
+    // Convierte la elección de la computadora en texto.
     if (opcionComputadora === 0) {
       opcionComputadora = "Piedra";
     } else if (opcionComputadora === 1) {
@@ -62,9 +72,10 @@ let iniciarJuego = () => {
     console.log("La computadora ha usado " + opcionComputadora);
     console.log("Usuario ha utilizado " + usuarioTexto);
 
+    // Función para determinar el resultado de la ronda.
     determinarResultado(usuarioTexto, opcionComputadora);
-    /************************************************************/
 
+    // Función para determinar el ganador.
     function determinarResultado(jugadaUsuario, jugadaComputadora) {
       if (resultados[jugadaUsuario].ganaA.includes(jugadaComputadora)) {
         puntosUsuario++;
@@ -88,8 +99,7 @@ let iniciarJuego = () => {
       }
     }
 
-    /************************************************************/
-
+    // Comprueba si un jugador ha alcanzado 5 puntos para declarar un ganador.
     if (puntosUsuario === 5) {
       GanaUsuario();
     } else if (puntosComputadora === 5) {
@@ -98,13 +108,12 @@ let iniciarJuego = () => {
   }
 };
 
-/************************************************************/
-
+// Función para buscar las rondas ganadas por un jugador en el historial.
 function buscarRondasGanadas(array, ganador) {
   return array.filter((ronda) => ronda.includes(ganador));
 }
 
-// Función Gana Usuario
+// Función que muestra el mensaje de victoria para el usuario.
 let GanaUsuario = () => {
   const rondasGanadasPorUsuario = buscarRondasGanadas(historialUsuaio, "Usuario");
   alert(`GANASTE LA PARTIDA:
@@ -114,7 +123,7 @@ let GanaUsuario = () => {
   ${rondasGanadasPorUsuario.join("\n")}`);
 };
 
-// Función Gana Computadora
+// Función que muestra el mensaje de victoria para la computadora.
 let GanaComputadora = () => {
   const rondasGanadasPorComputadora = buscarRondasGanadas(historialUsuaio, "Computadora");
   alert(`HAS PERDIDO LA PARTIDA:
